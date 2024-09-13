631e77345f7f4b7a4ef7fe56f02a7c9c82f722fc
bahaashahin <153877864+bahaashahin@users.noreply.github.com>
2024-09-13 17:38:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// function navigateToPage() {
+//     var selectedOption = document.getElementById("searchSelect").value;
+//     if (selectedOption === "courses") {
+//       window.location.href = "/courses"; // Redirect to Courses page
+//     } else if (selectedOption === "instructors") {
+//       window.location.href = "/instructors"; // Redirect to Instructors page
+//     } else if (selectedOption === "registered") {
+//       window.location.href = "/registered-courses"; // Redirect to Registered Courses page
+//     }
+//   }
+document.getElementById("joinButton").addEventListener("click", function() {
+  window.open("form.html", "_blank");
+});  
+document.getElementById("join").addEventListener("click", function() {
+  window.open("form.html", "_blank");
+});  
+document.getElementById("joinn").addEventListener("click", function() {
+  window.open("form.html", "_blank");
+});
